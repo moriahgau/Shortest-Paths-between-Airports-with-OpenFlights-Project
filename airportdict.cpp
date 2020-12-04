42900b4ad1d81b58fcf57b9eba8d7d99ddea025f
Gina Jiang <gjiang20@illinois.edu>
2020-12-03 23:29:15 -0600

    database in progress

---
+++ airportdict.cpp
@@ -0,0 +1,30 @@
+int Airport::getAirportData(string filename) { //airports.dat
+  ifstream myFile(filename); // Object to read from file
+  //myFile.open(filename, ios::in); // Opening file in input mode
+
+  if (myFile.is_open()) {}
+
+  while (getline(myFile, line)) {
+    stringstream ss(line);
+
+    //Airport(airportID, name, latitude, longitude);
+
+    /*
+    ifstream wordsFile(filename);
+    string word;
+    if (wordsFile.is_open()) {
+    // Reads a line from `wordsFile` into `word` until the file ends.
+      while (getline(wordsFile, word)) {
+        string temp = word;
+        std::sort(temp.begin(), temp.end());
+        dict[temp].push_back(word); //dict[word].push_back(line);
+      }
+    }
+    */
+
+  }
+
+  myFile.close();
+
+  return 0;
+}
