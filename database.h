acc5c68d73cdcad4bf579f216eb0a7e0866d5c5f
Gina Jiang <gjiang20@illinois.edu>
2020-12-07 01:01:34 -0600

    adding new structures and func for ease hopefully

--- database.h
+++ database.h
@@ -7,27 +7,53 @@ using namespace std;
 
 class Airport {
 public:
-  Airport(int id, string airportName, double lat, double long); //constructor
+  Airport(int id, string airportName, long double lat, long double longi); //constructor
 
   int airportID; //unique openflight's airport ID
   string name; //name of airport
-  double latitude; //Decimal degrees, usually to six significant digits. Negative is South, positive is North.
-  double longitude; //Decimal degrees, usually to six significant digits. Negative is South, positive is North.
+  long double latitude; //Decimal degrees, usually to six significant digits. Negative is South, positive is North.
+  long double longitude; //Decimal degrees, usually to six significant digits. Negative is South, positive is North.
 };
 
 class Database {
 public:
-  std::vector<Airport> all_airports;
-  std::map<int, std::vector<int>> connections;
+  vector<Airport> all_airports; //vector of all Airport objects in database
+  map<int, std::pair> locations; //map of locations of all Airports, locations[20].first = latitude, locations[20].second = longitude
+  map<int, vector<int>> connections; //map of < airportID, vector of connected airport IDs >
 
   //function declarations
-  //returns airport name given id
+
+  /*
+  ** Returns string name of Airport object given airportID.
+  */
   std::string getAiportName(int id);
 
-  //function to read CSV file and return a vector of all Airport objects
+  /*
+  ** Sorts 'all_airports' so that all_airports[0] is the starting point, and all_airports.back() is the ending point.
+  */
+  void sortAirportsVector(int startingID, int endingID);
+
+  /*
+  ** Read CSV file and store info in 'all_airports'.
+  ** 'all_airports' has all the Airports in database
+  **
+  ** example: can iterate through vector
+  ** for (auto i : all_airports) {
+  **  // 'i.airportID' to get airportID
+  ** }
+  */
   std::vector<Airport> getAirportInfo(string filename);
 
-  //returns map of 'airportID' with connected airportIDs
+  /*
+  ** Read CSV file and store info in 'connections'.
+  ** 'connections' has all the connected airport info in database
+  **
+  ** example: can iterate through map
+  ** for (auto i : connections) {
+  **  // 'i.first' to get the airportID (example: 20)
+  **  // 'i.second' to get the vector of airportIDs (example: <5,30,60>) connected to original airportID (example: 20)
+  ** }
+  */
   std::map<int, std::vector<int>> getConnections(string filename);
 
 
