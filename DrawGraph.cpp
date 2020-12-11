3667f366e889c829e57df5bb5d5ef8bed4dc78dc
Moriah Gau <mgau2@siebl-0224-20.ews.illinois.edu>
2020-12-11 17:10:02 -0600

    added comments to distance helper functions

--- DrawGraph.cpp
+++ DrawGraph.cpp
@@ -16,32 +16,32 @@
 #define pi 3.14159265358979323846
 using namespace std;
 //#include <bits/stdc++.h> 
-
+// helper function for distance calculation to convert to radians from degrees
 long double toRadians(const long double degree) 
 { 
-    long double one_deg = (M_PI) / 180; 
-    return (one_deg * degree); 
+    long double deg = (M_PI) / 180; 
+    deg = (deg * degree); 
+    return deg;
 } 
   
+// helper function to calculate distance using Haversine
 long double distance(long double lat1, long double long1,  
                      long double lat2, long double long2) 
 { 
+    //conversions from degrees to radians
     lat1 = toRadians(lat1);
     long1 = toRadians(long1); 
     lat2 = toRadians(lat2); 
     long2 = toRadians(long2); 
-    // Haversine
+    // Haversine formula to find distance between two locations
     long double dlong = long2 - long1;
     long double dlat = lat2 - lat1; 
-    long double ans = pow(sin(dlat / 2), 2) +  
-                          cos(lat1) * cos(lat2) *  
-                          pow(sin(dlong / 2), 2); 
-  
-    ans = 2 * asin(sqrt(ans)); 
-    long double R = 6371; 
-    ans = ans * R; 
-  
-    return ans; 
+    long double distance = pow(sin(dlat / 2), 2) + cos(lat1) * cos(lat2) * pow(sin(dlong / 2), 2); 
+    distance = 2 * asin(sqrt(distance)); 
+
+    long double radiusofearth = 6371; 
+    return distance * radiusofearth; 
+    // return ans; 
 } 
 /**
  * This function creates a graph, `g_` representing all the airports and their connections
